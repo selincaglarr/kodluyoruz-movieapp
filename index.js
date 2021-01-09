f8966fb1ad23fea4102422cab3a7bb9da6d63d50
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2021-01-09 22:44:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const FILM_API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
 let filmData;
 const getFilm = async () => {
